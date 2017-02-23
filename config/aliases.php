5cdc184bf6b61866e0093a053c5c1f23c80284cb
Курбан Курбанов <qurban.qranov93@gmail.com>
2017-02-24 02:19:04 +0400

    Initial commit

---
+++ config/aliases.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    '@foo' => '/path/to/foo',
+];
---
+++ views/layouts/main.php
@@ -0,0 +1,26 @@
+<?php
+use yii\helpers\Html;
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+<head>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <?= Html::csrfMetaTags() ?>
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
+</head>
+<body>
+<?php $this->beginBody() ?>
+
+<div class="wrap">
+
+    <div class="container">
+        <?= $content ?>
+    </div>
+</div>
+
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
